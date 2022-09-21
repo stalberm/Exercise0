eef8e9754159d3b0dd25d1b33d090a98f4022177
Micah <stalberm@oregonstate.edu>
2022-09-21 08:31:54 -0800

    First commit

---
+++ Exercise0.py
@@ -0,0 +1,28 @@
+def bot():
+
+    while True:
+        command = input("SuperMathyBot> ")
+        if command == "quit":
+            break
+
+        try:
+            command, v0, v1 = command.split()
+            if command not in ('add','sub','mul','div'):
+                raise
+            v0, v1 = float(v0), float(v1)
+        except:
+            print("usage: add|sub|mul|div v0 v1\n       quit")
+        else:
+            if command == 'add':
+                print(v0 + v1)
+            elif command == 'sub':
+                print(v0-v1)
+            elif command == 'mul':
+                print(v0*v1)
+            elif command == 'div':
+                if v1 == 0:
+                    print("Can't divide by zero.")
+                else:
+                    print(v0/v1)
+
+bot()
